2ee09d744f11e75c938035d26e6d5d4881d3e173
Kenny Hwu <kennywhwu@gmail.com>
2019-01-04 16:42:29 -0800

    added sliding window exercises

--- prob-sliding-window/find-longest-substring.js
+++ prob-sliding-window/find-longest-substring.js
@@ -1,4 +1,38 @@
 // add whatever parameters you deem necessary
-function findLongestSubstring() {}
+function findLongestSubstring(str) {
+  let start = 0,
+    maxLength = 0,
+    seen = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (seen[str[i]]) {
+      start = Math.max(start, seen[str[i]]);
+    }
+    maxLength = Math.max(maxLength, i - start + 1);
+    seen[str[i]] = i + 1;
+  }
+  return maxLength;
+}
+// // add whatever parameters you deem necessary
+// function findLongestSubstring(str) {
+//   if (str.length <= 1) return str.length;
+//   let start = 0,
+//     end = 0,
+//     maxLength,
+//     freq = {};
+
+//   while (end < str.length) {
+//     if (freq[str[end]]) {
+//       freq[str[start]]--;
+//       start++;
+//     } else {
+//       if (maxLength === undefined) maxLength = end - start + 1;
+//       maxLength = Math.max(maxLength, end - start + 1);
+//       freq[str[end]] = 1;
+//       end++;
+//     }
+//   }
+//   return maxLength;
+// }
 
 module.exports = findLongestSubstring;
--- prob-sliding-window/prob-sliding-window-solution/min-subarray-length.test.js
+++ prob-sliding-window/prob-sliding-window-solution/min-subarray-length.test.js
@@ -1,7 +1,7 @@
-const minSubarrayLength = require('./min-subarray-length');
+const minSubarrayLength = require("./min-subarray-length");
 
-describe('#minSubarrayLength', function() {
-  it('returns the min length', function() {
+describe("#minSubarrayLength", function() {
+  it("returns the min length", function() {
     expect(minSubarrayLength([2, 3, 1, 2, 4, 3], 7)).toBe(2);
     expect(minSubarrayLength([2, 1, 6, 5, 4], 9)).toBe(2);
     expect(minSubarrayLength([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)).toBe(
