395473a9abe738d6f10bd89673235b3acb5341bc
Kenny Hwu <kennywhwu@gmail.com>
2019-03-24 20:33:27 -0700

    revised find-kth-largest-element

--- find-kth-largest-element.js
+++ find-kth-largest-element.js
@@ -20,6 +20,32 @@ function findKthLargest(nums, k) {
     return arr;
   }
 
+  function _bubbleUp(arr) {
+    let idx = 0;
+    let leftIdx = 1;
+    let rightIdx = 2;
+    while (
+      idx < arr.length &&
+      (leftIdx < arr.length || rightIdx < arr.length)
+    ) {
+      let largest = idx;
+      leftIdx = idx * 2 + 1;
+      rightIdx = idx * 2 + 2;
+      if (arr[largest] > arr[leftIdx]) {
+        largest = leftIdx;
+      }
+      if (arr[largest] > arr[rightIdx]) {
+        largest = rightIdx;
+      }
+      if (idx !== largest) {
+        [arr[idx], arr[largest]] = [arr[largest], arr[idx]];
+        idx = largest;
+      } else {
+        break;
+      }
+    }
+  }
+
   for (let num of nums) {
     if (minHeap.length >= k) {
       if (num <= minHeap[0]) {
@@ -27,7 +53,9 @@ function findKthLargest(nums, k) {
       } else {
         minHeap.push(num);
         _sinkDown(minHeap);
-        minHeap.shift();
+        minHeap[0] = minHeap[minHeap.length - 1];
+        minHeap.pop();
+        _bubbleUp(minHeap);
       }
     } else {
       minHeap.push(num);
--- find-kth-largest-element.test.js
+++ find-kth-largest-element.test.js
@@ -6,5 +6,6 @@ describe("findKthLargest", () => {
     expect(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)).toEqual(4);
     expect(findKthLargest([0, 1, 2, 1, 0], 2)).toEqual(1);
     expect(findKthLargest([0, 1, 2, 2], 2)).toEqual(2);
+    expect(findKthLargest([-1, 2, 0], 2)).toEqual(0);
   });
 });
