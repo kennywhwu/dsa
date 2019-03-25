85a7f71d027ffac0dc252770475479ddfde14277
Kenny Hwu <kennywhwu@gmail.com>
2019-03-24 21:53:12 -0700

    revised zig-zag-conversion

--- zig-zag-conversion.js
+++ zig-zag-conversion.js
@@ -2,30 +2,50 @@
 
 // https://leetcode.com/problems/zigzag-conversion/
 
+// function convert(s, numRows) {
+//   if (s.length <= 2 || numRows <= 1 || numRows >= s.length) return s;
+//   let indexRef = {};
+//   for (let i = 0; i < s.length; i++) {
+//     let idx = i % (numRows - 1);
+//     if (Math.floor(i / (numRows - 1)) % 2 === 0) {
+//       if (indexRef[idx]) {
+//         indexRef[idx] += s[i];
+//       } else {
+//         indexRef[idx] = s[i];
+//       }
+//     } else {
+//       if (indexRef[numRows - idx - 1]) {
+//         indexRef[numRows - idx - 1] += s[i];
+//       } else {
+//         indexRef[numRows - idx - 1] = s[i];
+//       }
+//     }
+//   }
+//   let output = "";
+//   for (let i = 0; i < numRows; i++) {
+//     output += indexRef[i];
+//   }
+//   return output;
+// }
+
 function convert(s, numRows) {
-  if (s.length <= 2 || numRows <= 1 || numRows >= s.length) return s;
-  let indexRef = {};
-  for (let i = 0; i < s.length; i++) {
-    let idx = i % (numRows - 1);
-    if (Math.floor(i / (numRows - 1)) % 2 === 0) {
-      if (indexRef[idx]) {
-        indexRef[idx] += s[i];
-      } else {
-        indexRef[idx] = s[i];
-      }
+  let output = [];
+  let idx = 0;
+  let step = 1;
+  for (let char of s) {
+    if (output[idx]) {
+      output[idx] += char;
     } else {
-      if (indexRef[numRows - idx - 1]) {
-        indexRef[numRows - idx - 1] += s[i];
-      } else {
-        indexRef[numRows - idx - 1] = s[i];
-      }
+      output[idx] = char;
     }
+    if (idx === 0) {
+      step = 1;
+    } else if (idx === numRows - 1) {
+      step = -1;
+    }
+    idx += step;
   }
-  let output = "";
-  for (let i = 0; i < numRows; i++) {
-    output += indexRef[i];
-  }
-  return output;
+  return output.join("");
 }
 
 module.exports = convert;
