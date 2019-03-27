69214adf30711c8eb3207f0f3cacad04338b0d6d
Kenny Hwu <kennywhwu@gmail.com>
2019-03-27 16:07:09 -0700

    refactored binary-tree-flatten

--- binary-tree-flatten.js
+++ binary-tree-flatten.js
@@ -2,20 +2,34 @@
 
 // https://leetcode.com/problems/flatten-binary-tree-to-linked-list
 
+// function flatten(root) {
+//   if (root === null) return [];
+//   let list = [];
+//   function _DFS(node) {
+//     if (node === null) return;
+//     list.push(node);
+//     _DFS(node.left);
+//     _DFS(node.right);
+//   }
+//   _DFS(root);
+//   for (let i = 0; i < list.length - 1; i++) {
+//     list[i].left = null;
+//     list[i].right = list[i + 1];
+//   }
+//   return root;
+// }
 function flatten(root) {
   if (root === null) return [];
-  let list = [];
+  let prev = null;
   function _DFS(node) {
     if (node === null) return;
-    list.push(node);
-    _DFS(node.left);
     _DFS(node.right);
+    _DFS(node.left);
+    node.right = prev;
+    node.left = null;
+    prev = node;
   }
   _DFS(root);
-  for (let i = 0; i < list.length - 1; i++) {
-    list[i].left = null;
-    list[i].right = list[i + 1];
-  }
   return root;
 }
 
