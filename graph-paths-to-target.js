5cbcbfeda94529e165743dbf60e32d54f5ecc576
Kenny Hwu <kennywhwu@gmail.com>
2019-04-01 17:56:37 -0700

    revised graph-paths-to-target does not need visited due to acyclic graph

--- graph-paths-to-target.js
+++ graph-paths-to-target.js
@@ -5,20 +5,15 @@
 var allPathsSourceTarget = function(graph) {
   let paths = [];
   let currentPath = [0];
-  let visited = new Set().add(0);
 
   function _DFS(node) {
     if (node === graph.length - 1) {
       paths.push([...currentPath]);
     } else {
       graph[node].forEach(e => {
-        if (!visited.has(e)) {
-          visited.add(e);
-          currentPath.push(e);
-          _DFS(e);
-          visited.delete(e);
-          currentPath.pop();
-        }
+        currentPath.push(e);
+        _DFS(e);
+        currentPath.pop();
       });
     }
   }
