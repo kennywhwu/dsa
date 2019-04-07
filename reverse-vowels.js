3c1933668e09873488db60a79d966d871a9a5435
Kenny Hwu <kennywhwu@gmail.com>
2019-04-06 23:55:15 -0700

    optimized reverse-vowels

--- reverse-vowels.js
+++ reverse-vowels.js
@@ -4,19 +4,36 @@
 
 function reverseVowels(s) {
   const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
-  let vowelRef = [];
-  for (let i = 0; i < s.length; i++) {
-    if (vowels.has(s[i])) vowelRef.push(i);
-  }
+  let start = 0;
+  let end = s.length - 1;
   let strArr = s.split("");
-  for (let i = 0; i < vowelRef.length / 2; i++) {
-    [strArr[vowelRef[i]], strArr[vowelRef[vowelRef.length - 1 - i]]] = [
-      strArr[vowelRef[vowelRef.length - 1 - i]],
-      strArr[vowelRef[i]],
-    ];
+  while (start < end) {
+    while (start < end && !vowels.has(strArr[start])) {
+      start++;
+    }
+    while (start < end && !vowels.has(strArr[end])) {
+      end--;
+    }
+    [strArr[start], strArr[end]] = [strArr[end], strArr[start]];
+    start++;
+    end--;
   }
-
   return strArr.join("");
+
+  // const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  // let vowelRef = [];
+  // for (let i = 0; i < s.length; i++) {
+  //   if (vowels.has(s[i])) vowelRef.push(i);
+  // }
+  // let strArr = s.split("");
+  // for (let i = 0; i < vowelRef.length / 2; i++) {
+  //   [strArr[vowelRef[i]], strArr[vowelRef[vowelRef.length - 1 - i]]] = [
+  //     strArr[vowelRef[vowelRef.length - 1 - i]],
+  //     strArr[vowelRef[i]],
+  //   ];
+  // }
+
+  // return strArr.join("");
 }
 
 module.exports = reverseVowels;
