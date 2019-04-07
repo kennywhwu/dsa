66779061ed443439b4ede3c07f8e0e8bae2f320a
Kenny Hwu <kennywhwu@gmail.com>
2019-04-06 21:23:14 -0700

    optimized trapping-rain-water

--- trapping-rain-water.js
+++ trapping-rain-water.js
@@ -1,31 +1,57 @@
 // Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining (https://leetcode.com/problems/trapping-rain-water/)
 
-var trap = function(height) {
+// https://leetcode.com/problems/trapping-rain-water/
+
+function trap(height) {
   let sum = 0;
-  for (let i = 0; i < height.length; i++) {
-    if (height[i] === 0) continue;
-    let max = 0;
-    for (let j = i + 1; j < height.length; j++) {
-      if (height[j] >= height[i]) {
-        sum += (j - i - 1) * (height[i] - max);
-        break;
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+  while (left <= right) {
+    if (height[left] <= height[right]) {
+      if (height[left] > leftMax) {
+        leftMax = height[left];
       } else {
-        max = Math.max(max, height[j]);
+        sum += leftMax - height[left];
       }
-    }
-    max = 0;
-    for (let j = i - 1; j >= 0; j--) {
-      if (height[j] > height[i]) {
-        sum += (i - j - 1) * (height[i] - max);
-        break;
-      } else if (height[j] === height[i]) {
-        break;
+      left++;
+    } else {
+      if (height[right] > rightMax) {
+        rightMax = height[right];
       } else {
-        max = Math.max(max, height[j]);
+        sum += rightMax - height[right];
       }
+      right--;
     }
   }
   return sum;
-};
+
+  // let sum = 0;
+  // for (let i = 0; i < height.length; i++) {
+  //   if (height[i] === 0) continue;
+  //   let max = 0;
+  //   for (let j = i + 1; j < height.length; j++) {
+  //     if (height[j] >= height[i]) {
+  //       sum += (j - i - 1) * (height[i] - max);
+  //       break;
+  //     } else {
+  //       max = Math.max(max, height[j]);
+  //     }
+  //   }
+  //   max = 0;
+  //   for (let j = i - 1; j >= 0; j--) {
+  //     if (height[j] > height[i]) {
+  //       sum += (i - j - 1) * (height[i] - max);
+  //       break;
+  //     } else if (height[j] === height[i]) {
+  //       break;
+  //     } else {
+  //       max = Math.max(max, height[j]);
+  //     }
+  //   }
+  // }
+  // return sum;
+}
 
 module.exports = trap;
